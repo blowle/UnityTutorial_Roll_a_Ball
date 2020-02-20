555cbc9891ecaa5048cfa3a62e443e6b6adc466e
YongCheol Lee <dydcjf8889@gmail.com>
2020-02-21 00:45:51 +0900

    First Commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // when game start, store offset of camera's position based on the player.
+        offset = transform.position - player.transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerContorller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContorller : MonoBehaviour
+{
+
+    public float speed;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    // therefore, usually it is used in order to initialize variables.
+    void Start()
+    {
+        // get rigidbody component that this object takes.
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal"); // check <-, -> arrow key.
+        float vertical = Input.GetAxis("Vertical");     // check up, down arraow key.
+
+        // store a vector value inputed by arrow keys.
+        Vector3 movement = new Vector3(horizontal, 0f, vertical);
+
+        // force movement to Rigidbody.
+        rb.AddForce(movement * speed);
+    }
+}

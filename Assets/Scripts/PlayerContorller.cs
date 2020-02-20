2c633849d88c2c66cff6fee2e4ddb59195ea7deb
YongCheol Lee <dydcjf8889@gmail.com>
2020-02-21 02:19:04 +0900

    complete first Unity Tutorial - Roll a Ball.

--- Assets/Scripts/PlayerContorller.cs
+++ Assets/Scripts/PlayerContorller.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerContorller : MonoBehaviour
 {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     Rigidbody rb;
+    int count;
 
     // Start is called before the first frame update
     // therefore, usually it is used in order to initialize variables.
@@ -15,6 +20,10 @@ public class PlayerContorller : MonoBehaviour
     {
         // get rigidbody component that this object takes.
         rb = GetComponent<Rigidbody>();
+
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     // Update is called once per frame
@@ -33,5 +42,29 @@ public class PlayerContorller : MonoBehaviour
 
         // force movement to Rigidbody.
         rb.AddForce(movement * speed);
+    }
+
+    // when collision, Trigger Collider
+    void OnTriggerEnter(Collider other)
+    {
+        // if a collided Collider  have a tag called "Item",
+        if (other.tag == "Item")
+        {
+            // then, this collider changes a state to inactive.
+            other.gameObject.SetActive(false);
+
+            ++count;
+            SetCountText();
+
+            if(count >= 7)
+            {
+                winText.text = "You Win!";
+            }
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count : " + count.ToString();
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # UnityTutorial_Roll_a_Ball
 Unity Study - 유니티 튜토리얼 : Roll a ball 
+
+- ### * 2일 차 
+
+  1.  Ball 이 사각형 형태의 아이템과 접촉 시 획득하는 방법을 배워보자.
+  2. 획득 수 및 완료 했을 때의 텍스트를 표현.
+  3. 완성 했으면, 빌드를 해보자.
+
